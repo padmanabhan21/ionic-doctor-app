f4e4b4712ff322f7cb140968a2efe1d4af0a45d3
padmanabhan21 <padmanabhan210193@gmail.com>
2019-02-09 15:19:40 +0530

    login screen

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,34 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
-
+  backgrounds = [
+    'assets/imgs/background/background-1.jpg',
+    'assets/imgs/background/background-2.jpg',
+    'assets/imgs/background/background-3.jpg',
+    'assets/imgs/background/background-4.jpg'
+  ];
+  public loginForm: any;
+    
+  constructor(public navCtrl: NavController,public formBuilder: FormBuilder) {
+    this.loginForm = formBuilder.group({
+      email: ['', Validators.required],
+      password: ['', Validators.compose([Validators.minLength(6),
+        Validators.required])]
+    });
   }
 
+  openResetPassword() {
+    console.log('Reset password clicked');
+  }
+
+  doLogin() {
+    if (!this.loginForm.valid) {
+      console.log('Invalid or empty data');
+    } else {
+      const userEmail = this.loginForm.value.email;
+      const userPassword = this.loginForm.value.password;
+
+      console.log('user data', userEmail, userPassword);
+    }
+  }
 }
